2b1152ff76fcb67c5c96d84258f9d378df20bd6f
Antonio <Antonio@DESKTOP-R5V1EB6>
2024-01-07 18:11:38 +0100

    Agregar archivos de proyecto.

---
+++ MVVM/Services/TelefonosService.cs
@@ -0,0 +1,18 @@
+﻿using MVVM_DEMO.Model;
+
+namespace MVVM_DEMO.Services
+{
+    public class TelefonosService
+    {
+        private Telefono tado;
+    
+        static public void TelefonosServiceInsertar(Telefono t)
+        {
+            using (TelefonosADO tado = new TelefonosADO())
+            {
+                tado.Insertar(t);
+            }
+        }
+
+    }
+}
---
+++ MVVM/ViewModel/TelefonosViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MVVM_DEMO.Model;
+using MVVM_DEMO.ViewModel.Base;
+using MVVM_DEMO.Services;
+
+namespace MVVM_DEMO.ViewModel
+{
+    public class TelefonosViewModel : BaseViewModel
+    {
+        private string ntelefono;
+        private string nombre;
+        private string direccion;
+        private string observaciones;
+
+        public RelayCommand GuardarCommand => new RelayCommand(execute=>PerformSaveTelefonos(), canExecute=>CanExecuteSaveTelefonos());
+        public TelefonosViewModel()
+        {
+        
+        }
+        public string Ntelefono
+        {
+            get { return ntelefono; }
+            set { ntelefono = value; OnPropertyChanged(); }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; OnPropertyChanged(); }
+        }
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; OnPropertyChanged(); }
+        }
+        public string Observaciones
+        {
+            get { return observaciones; }
+            set { observaciones = value; OnPropertyChanged(); }
+        }
+        private bool CanExecuteSaveTelefonos()
+        {
+            return !string.IsNullOrEmpty(Ntelefono);
+        }
+        private void PerformSaveTelefonos() =>
+           TelefonosService.TelefonosServiceInsertar(new Telefono(ntelefono, nombre, direccion, observaciones));    
+    }
+}
